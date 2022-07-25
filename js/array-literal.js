1f70a84e504a89d63752c9b7a05931a7c2285d52
phamvanduc05041996 <106530245+phamvanduc05041996@users.noreply.github.com>
2022-07-25 21:07:00 +0700

    123

---
+++ js/array-literal.js
@@ -0,0 +1,7 @@
+//Crate an array and assign it values.
+var colors;
+colors  =  ['white', 'black','custom'];
+
+//Show the first item from the array.
+var el =document.getElementById('color');
+el.textContent=colors[0];
